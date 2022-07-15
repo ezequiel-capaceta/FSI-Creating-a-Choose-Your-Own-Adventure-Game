af35d0f3baaf525156f4cee105124058692c0f53
ezequiel-capaceta <108444379+ezequiel-capaceta@users.noreply.github.com>
2022-07-14 20:37:51 -0700

    follow up on luis' steps

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//Your Code here
+let response = window.prompt("Do you go left of right?")
+
+console.log(response)
+
+if(response === "left"){
+
+    console.log("user submitted left")
+
+    let response2 = window.prompt("Do you go up or down?")
+
+    if( response2 === "up") {
+        console.log("user submitted up")
+    } else if ( response2 === "down") {
+        console.log("user submitted down")
+
+    }
+
+
+} else if (response === "right") {
+    console.log("user submitted right")
+}
